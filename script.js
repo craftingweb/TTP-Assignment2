9fc562924e81a0e171f7979248420dbeb460410b
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2023-06-19 09:18:00 -0400

    second half

--- script.js
+++ script.js
@@ -83,7 +83,109 @@ console.log(myEvery(arr, callbackEvery));
 //
 //
 //
+console.log("Reduce:");
 
+function callbackReduce(num) {
+  return num;
+}
+
+function myReduce(arr, callbackReduce) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += callbackReduce(arr[i]);
+  }
+  return sum;
+}
+
+console.log(myReduce(arr, callbackReduce));
+//
+//
+//
+console.log("Includes:");
+
+function myIncudes(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(myIncudes(arr, 2));
+//
+//
+//
+console.log("Index of: ");
+
+function myIndexOf(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == target) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(myIndexOf(arr, 36));
+//
+//
+//
+console.log("Push: ");
+
+function myPush(arr, element) {
+  let pushArray = [...arr, element];
+  return pushArray;
+}
+
+console.log(myPush(arr, 23));
+//
+//
+//
+console.log("Last Index of: ");
+
+function lastIndexOf(arr, target) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return false;
+}
+
+console.log(lastIndexOf(arr, 10));
+//
+//
+//
+console.log("Keys: ");
+
+let obj = { name: "John", age: 20, color: "blue" };
+
+function grabKeys(obj) {
+  let arrayKeys = [];
+  for (let i in obj) arrayKeys.push(i);
+  return arrayKeys;
+}
+
+console.log(grabKeys(obj));
+//
+//
+//
+console.log("Values: ");
+
+function grabValues(object) {
+  let arrValues = [];
+  for (let value in object) {
+    arrValues.push(object[value]);
+  }
+  return arrValues;
+}
+
+console.log(grabValues(obj));
+
+//
+//
+//
 console.log("Zeros:");
 let nums = [0, 1, 0, 3, 12];
 function moveZeros(nums) {
